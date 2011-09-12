41a9e610368529cc8c7685bec195c91cafa68a2c
Neil <Neil@192.168.2.51>
2011-09-12 21:57:11 +0800

    new version..

---
+++ admin/manage-accounts.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/template-husai.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- InstanceEndEditable -->
+<link rel="stylesheet" type="text/css" href="../style.css" media="screen" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+
+	<div id = "wrapper">
+    
+    	<div id = "header-left">  <img src = "../images/purplelogo.jpg">  
+        	
+        </div>
+        
+        <div id = "header-right">Husai Health & Beauty Haven</div>
+    
+    	<div id = "upper-nav-border"> <img src = "../images/purplebottom.png"> </div>
+    	<!-- InstanceBeginEditable name="navigation" -->
+    	<div id="nav">
+    	  <ul>
+    	    <li><a href = "manage-home.php">Home</a></li>
+    	    <li><a href = "manage-services-new.php">Services</a></li>
+    	    <li><a href = "manage-products-new.php">Products</a></li>
+    	    <li><a href = "manage-about.php">About</a></li>
+            <li><a href = "manage-accounts.php">Accounts</a></li>
+  	      </ul>
+  	    </div>
+    	<!-- InstanceEndEditable -->
+    	<div id = "upper-nav-border"> <img src = "../images/purplebottom.png"> </div>
+    <!-- InstanceBeginEditable name="content-left" -->
+        <div id = "content-left">
+       	  <table id = "page-title">
+            	<tr>
+                	<td> <img src = "../images/purpletitle.png" /></td>
+                    <td> Accounts </td>
+                </tr>
+            </table>
+        	<p>&nbsp;</p>
+            <p id = "subtitle">Admin Account</p>
+            <div id = "admin-prof">
+  				<table width="345" cellspacing="15">
+                    <tr>
+                        <td width="99" align="right">Admin ID: </td>
+                        <td width="212">ADMIN</td>
+                    </tr>
+                    <tr>
+                        <td align="right">Password: </td>
+                        <td><a href = "">[Change Password]</a></td>
+                    </tr>    
+                                             
+                </table>
+            </div> 
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p id = "add-customer"><a href = "add-customer-account.php">Add Customer Account</a></p>
+            <p>&nbsp;</p>
+            <p id = "header-customer">Customers Accounts</p>          
+            <table id = "customer-table" cellspacing="10">
+                    <th width="187">Customer Name</th>
+                    <th width="135">User ID</th>
+                    <th width="75">Status</th>
+                    
+                    <tr>
+                    	<td>John David Washington Baltazar</td>
+                        <td>JDB</td>
+                        <td>Activated</td>
+                    </tr>
+    
+           </table>        
+            
+        </div>
+    <!-- InstanceEndEditable --><!-- InstanceBeginEditable name="content-right" -->
+        <div id = "content-right"> 
+        	
+
+        </div>
+        <!-- InstanceEndEditable -->
+        <div id = "footer">
+        	<p id="fw-foottext" class="fw-footertext">Copyright ©2011</p> 
+        </div>
+    
+    </div>
+    
+</body>
+<!-- InstanceEnd --></html>
---
+++ admin/manage-products-new.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/template-husai.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- InstanceEndEditable -->
+<link rel="stylesheet" type="text/css" href="../style.css" media="screen" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+
+	<div id = "wrapper">
+    
+    	<div id = "header-left">  <img src = "../images/purplelogo.jpg">  
+        	
+        </div>
+        
+        <div id = "header-right">Husai Health & Beauty Haven</div>
+    
+    	<div id = "upper-nav-border"> <img src = "../images/purplebottom.png"> </div>
+    	<!-- InstanceBeginEditable name="navigation" -->
+    	<div id="nav">
+    	  <ul>
+    	    <li><a href = "manage-home.php">Home</a></li>
+    	    <li><a href = "manage-services-new.php">Services</a></li>
+    	    <li><a href = "manage-products-new.php">Products</a></li>
+    	    <li><a href = "manage-about.php">About</a></li>
+            <li><a href = "manage-accounts.php">Accounts</a></li>
+  	      </ul>
+  	    </div>
+    	<!-- InstanceEndEditable -->
+    	<div id = "upper-nav-border"> <img src = "../images/purplebottom.png"> </div>
+    <!-- InstanceBeginEditable name="content-left" -->
+        <div id = "content-left">
+        	   <table id = "page-title">
+            	<tr>
+                	<td> <img src = "../images/purpletitle.png" /></td>
+                    <td>Manage Products &raquo; What's New?</td>
+                </tr>
+            </table>
+            <p>&nbsp;</p>
+        
+            <p id = "subtitle">ADD NEW PRODUCTS</p>
+            <form method = "post" action = "" >
+              <table>
+                    <tr>
+                        <td align="right">Product Name: </td>
+                        <td><input type = "text" name = "service-name"/ id = "text-field"></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Price: </td>
+                        <td><input type = "text" name = "service-price" id = "text-field"/></td>
+                    </tr>
+                    
+                    <tr>
+                        <td align="right">Description: </td>
+                        <td><input type = "text" name = "service-desc" id = "text-field"/></td>
+                    </tr>
+                     <tr>
+                        <td align="right">Image: </td>
+                        <td><input type = "text" name = "service-image" id = "text-field"/></td>
+                        <td><input type = "submit" value = "Upload" id = "upload"></td>
+                    </tr>
+                     <tr>
+                        <td>&nbsp;</td>
+                        <td></td>               		
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td align="center"> <input type = "submit" value = "Save" id = "upload"/></td>               		
+                        <td>&nbsp;</td>
+                    </tr>
+                      
+                </table>
+                  <p>&nbsp;</p>
+               
+            </form>
+
+        </div>
+    <!-- InstanceEndEditable --><!-- InstanceBeginEditable name="content-right" -->
+        <div id = "content-right"> 
+        	<div>            	
+            	<ul>
+                	<li><a href = "manage-products-new.php">&raquo; What's New?</a></li>
+                    <li><a href = "manage-products-special.php">&raquo; Special / Discounted Offers</a></li>
+                    <li><a href = "manage-products-face.php">&raquo; Face</a></li>
+                    <li><a href = "manage-products-body.php">&raquo; Body</a></li>
+                    <li><a href = "manage-products-hair.php">&raquo; Hair</a></li>
+                    
+                </ul>
+            </div> 
+
+        </div>
+        <!-- InstanceEndEditable -->
+        <div id = "footer">
+        	<p id="fw-foottext" class="fw-footertext">Copyright ©2011</p> 
+        </div>
+    
+    </div>
+    
+</body>
+<!-- InstanceEnd --></html>
